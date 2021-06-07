855bd70bdad0d046fdaca5abcb8468b6cfe1aeb0
Hazel Bounpraseuth <78515992+bounpraseuthh9003@users.noreply.github.com>
2021-06-08 10:48:33 +1200

    user choice - chosen operator to play with

--- 02_user_choice_v1.py
+++ 02_user_choice_v1.py
@@ -1,7 +1,5 @@
 # BF Component 1 - Getting input from user (what component they would like to play with)
 
-result = ""
-
 
 # Functions goes here...
 def choice_checker(question):
@@ -9,37 +7,35 @@ def choice_checker(question):
     valid = False
     while not valid:
 
-        # Ask users for choice
+        # Ask users for their choice (what component they would like to play with)
         response = input(question)
 
         if response == "1":
-            result == "+"
-            continue
+            response = "+"
+            return response
         elif response == "2":
-            result == "-"
-            continue
+            response = "-"
+            return response
         elif response == "3":
-            result == "x"
-            continue
+            response = "x"
+            return response
         elif response == "4":
-            result == "÷"
-            continue
+            response = "÷"
+            return response
         else:
             print("Please choose a number between 1-4")
-
-        return response
+            print()
 
 # Main routine goes here
 
 # Ask user their choice of ( + , - , x , / ) - check if this is valid
 
-
 # print out choice for comparison purposes
-user_choice = choice_checker("Choose 1 for ' + '\n"
-                             "Choose 2 for ' - '\n"
-                             "Choose 3 for ' x '\n"
-                             "Choose 4 for ' ÷ '\n"
-                             "\n"
-                             "= ")
-
-print("You chose {}".format(result))
+for item in range(0, 5):    # run 5 times so I can test all possible options
+    user_choice = choice_checker("Choose 1 for ' + '\n"
+                                 "Choose 2 for ' - '\n"
+                                 "Choose 3 for ' x '\n"
+                                 "Choose 4 for ' ÷ '\n"
+                                 "\n"
+                                 "= ")
+    print("You chose {}".format(user_choice))
