0c600ad344b3139b8e167647b90b8c96d2b2e6cd
Hazel Bounpraseuth <78515992+bounpraseuthh9003@users.noreply.github.com>
2021-06-10 14:48:59 +1200

    user choice - chosen operator to play with

--- 02_user_choice_v2.py
+++ 02_user_choice_v2.py
@@ -1,26 +1,42 @@
-# Functions go here
+# BF Component 1 - Getting input from user (what component they would like to play with)
+# Version 2 - Checks that response is in a given list
 
+
+# Functions goes here...
+def choice_checker(question, options):
+    valid = False
+    while not valid:
+
+        # Ask users for their choice (what component they would like to play with)
+        response = input(question).lower()
+
+        if response == "a":
+            response = "addition"
+        elif response == "s":
+            response = "subtraction"
+        elif response == "m":
+            response = "multiplication"
+        elif response == "d":
+            response = "division"
+
+        if response in options:
+            return response
+        else:   # if not in list, says to choose a valid letter
+            print("Please choose a valid letter ( A - Addition, S - Subtraction, M - Multiplication, D - Division)")
+            print()
 
 # Main routine goes here
+math_list = ["addition", "subtraction", "multiplication", "division"]
 
-error = " !!!Please enter an whole number between 1 and 20!!! "
+# Ask user their choice of ( + , - , x , / ) - check if this is valid
 
-valid = False
-while not valid:
-    try:
-        #   ask the question
-        print()
-        response = int(input("How many questions would you like ? "))
-
-        # if the amount is too low ? too high give
-        if 0 < response <= 20:
-            print("You have asked for {} questions " . format(response))
-
-        # output an error
-        else:
-            print()
-            print(error)
-
-    except ValueError:
-            print()
-            print(error)
+# print out choice for comparison purposes
+for item in range(0, 8):  # run 8 times so I can test all possible options, loops for easier testing
+    user_choice = choice_checker("Which one would you like to play with?\n"
+                                 "A - Addition\n"
+                                 "S - Subtraction\n"
+                                 "M - Multiplication\n"
+                                 "D - Division\n"
+                                 "= ", math_list)
+    print("You chose {}".format(user_choice))
+    print()
