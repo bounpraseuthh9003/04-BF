1c5a778c337a9385914951cd198902043ba263c6
Hazel Bounpraseuth <78515992+bounpraseuthh9003@users.noreply.github.com>
2021-06-14 12:20:33 +1200

    sandpit

--- 00_BF_sandpit.py
+++ 00_BF_sandpit.py
@@ -1,45 +1,80 @@
-# BF Component 1 - Getting input from user (what component they would like to play with)
+# BF Component 2 - Computer generates random questions based on chosen generator
+# Version 1
 
-result = ""
+from random import randint
 
+# values
+num_1 = ""
+num_2 = ""
 
-# Functions goes here...
-def choice_checker(question):
+# functions
+def addition():
 
-    valid = False
-    while not valid:
+    num_1 = randint(0,10)
+    num_2 = randint(0,10)
 
-        # Ask users for choice
-        response = input(question)
+    print(f"What is {num_1} + {num_2} ?")
+    choice = (input("= "))
 
-        if response == "1":
-            result == "+"
-            continue
-        elif response == "2":
-            result == "-"
-            continue
-        elif response == "3":
-            result == "x"
-            continue
-        elif response == "4":
-            result == "÷"
-            continue
-        else:
-            print("Please choose a number between 1-4")
+    if int(choice) == num_1 + num_2:
+      print("correct, nice job! keep on playing")
+    else:
+      print(f"Incorrect... the answer is {num_1 + num_2}")
 
-        return response
+def subtraction():
 
-# Main routine goes here
+    num_1 = randint(0,10)
+    num_2 = randint(0,10)
 
-# Ask user their choice of ( + , - , x , / ) - check if this is valid
+    print(f"What is {num_1} - {num_2} ?")
+    choice = (input("= "))
 
+    if int(choice) == num_1 - num_2:
+      print("correct, nice job! keep on playing")
+    else:
+      print(f"Incorrect... the answer is {num_1 - num_2}")
 
-# print out choice for comparison purposes
-user_choice = choice_checker("Choose 1 for ' + '\n"
-                             "Choose 2 for ' - '\n"
-                             "Choose 3 for ' x '\n"
-                             "Choose 4 for ' ÷ '\n"
-                             "\n"
-                             "= ")
+def multiplication():
 
-print("You chose {}".format(result))
+    num_1 = randint(0,10)
+    num_2 = randint(0,10)
+
+    print(f"What is {num_1} x {num_2} ?")
+    choice = (input("= "))
+
+    if int(choice) == num_1 * num_2:
+      print("correct, nice job! keep on playing")
+    else:
+      print(f"Incorrect... the answer is {num_1 * num_2}")
+
+def division():
+
+    num_1 = randint(0,10)
+    num_2 = randint(0,10)
+
+    print(f"What is {num_1} / {num_2} ?")
+    choice = (input("= "))
+
+    if int(choice) == num_1 / num_2:
+      print("correct, nice job! keep on playing")
+    else:
+      print(f"Incorrect... the answer is {num_1 / num_2}")
+
+# main routine
+choice = input("Which one would you like to play with?\n"
+"A - Addition\n"
+"S - Subtraction\n"
+"M - Multiplication\n"
+"D - Division\n"
+"= ")
+
+if choice == "a":
+  addition()
+elif choice == "s":
+  subtraction()
+elif choice == "m":
+  multiplication()
+elif choice == "d":
+  division()
+else:
+  print("Please choose a letter ( A , S , M , D")
