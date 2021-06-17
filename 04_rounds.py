41c143bdf44328fb2e64239064555d93bf4fc362
Hazel Bounpraseuth <78515992+bounpraseuthh9003@users.noreply.github.com>
2021-06-17 14:50:14 +1200

    round mechanics

--- 04_rounds.py
+++ 04_rounds.py
@@ -1,42 +1,40 @@
 # There will be 10 questions, users should be able to quit anytime using 'xxx' and then display results.
 # Users can also play infinite mode where they can press 'xxx' to quit anytime
 
-# Version 1 - Makes sure the questions chosen by user is valid and is not too low or too high
+# Version 1 - Makes sure the questions chosen by user is valid and is not too low or too high + continuous
 
 
 # Functions
-
-
 def get_rounds():
 
-    error = "Please enter an integer between 1-10"
+    error = "Please enter an integer more than 0 OR press <Enter> for infinite mode."
 
     valid = False
-
     while not valid:
         try:
-            #   ask the question
+            # ask the question
             print()
-            response = int(input("How many questions would you like between 1-10? "))
+            response = int(input("How many questions would you like? \n"
+                                 "You can press <Enter> for infinite mode \n"
+                                 "= "))
 
-            if 1 < response <= 10:
+            if response == "":
+                return ("continuous")
+
+            # if response is higher than 2 say they have asked for that amount of questions with the plural(s)
+            if response >= 2:
                 print("You have asked for {} questions ".format(response))
+
             # if response is 1, say "1 question" instead of "questions"
             elif response == 1:
                 print("You have asked for 1 question")
-            # return response if user chooses more than 0 questions
-            elif response > 0:
-                return response
 
-            # output an error
             else:
                 print()
                 print(error)
 
         except ValueError:
-            print()
             print(error)
 
-# Main Routine
-for item in range(0,5):  # run 8 times so I can test all possible options, loops for easier testing
-    rounds = get_rounds(int("How many questions would you like between 1-10? "))
+num_rounds = get_rounds()
+print(num_rounds)
--- 04_rounds_v2.py
+++ 04_rounds_v2.py
@@ -6,33 +6,29 @@
 
 # Functions
 
-
 def get_rounds():
 
-    error = "Please enter an integer between 1-10"
+  error = "Please enter an integer or press <enter>"
 
-    valid = False
+  valid = False
 
-    while not valid:
-        try:
-            #   ask the question
-            print()
-            response = int(input("How many questions would you like between 1-10? \n"
-                                 "You can press <Enter> for continuous mode: \n"))
-            if response == "":
-                return ("continuous")
+  while not valid:
+    print()
+    response = input("How many rounds? \nPress <enter> for continuous mode: \n")
 
-            if 1 < response <= 10:
-                print("You have asked for {} questions ".format(response))
-            # if response is 1, say "1 question" instead of "questions"
-            elif response == 1:
-                print("You have asked for 1 question")
+    if response == "":
+      return("continuous")
 
-            # output an error
-            else:
-                print()
-                print(error)
+    try:
+      response = int(response)
 
-        except ValueError:
-            print()
-            print(error)
+      if response > 0:
+        return response
+      else:
+        print(error)
+
+    except ValueError:
+      print(error)
+
+num_rounds = get_rounds()
+print(num_rounds)
