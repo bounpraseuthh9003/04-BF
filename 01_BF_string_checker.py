346d29934b94ec25ab02470e568c11da967f7022
Hazel Bounpraseuth <78515992+bounpraseuthh9003@users.noreply.github.com>
2021-06-22 10:46:57 +1200

    string checker

--- 01_BF_string_checker.py
+++ 01_BF_string_checker.py
@@ -1,26 +1,36 @@
+# string checker, checks user input is valid
 
+
+# Checks user input (either full word or first letter of word)
 def string_checker(question, to_check):
+
     valid = False
     while not valid:
-
+        # ask user question and change response to lowercase
         response = input(question).lower()
 
+        if response == "xxx":
+            return response
+
+        # check response is in list OR that it's the first letter of an item in the list
         for item in to_check:
             if response == item:
                 return response
             elif response == item[0]:
                 return item
 
+        # if item not in list, print error
         print("sorry that is not a valid response")
 
 
 # *** Main Routine starts here ***
 
 yes_no = ["yes", "no"]
-rps = ["rock", "paper", "scissors"]
+math_list = ["addition", "subtraction", "multiplication", "division"]
 
 mood = string_checker("Are you happy? ", yes_no)
 print(mood)
+print()
 
-choose = string_checker("Choose: ", rps)
+choose = string_checker("Choose: ", math_list)
 print(choose)
